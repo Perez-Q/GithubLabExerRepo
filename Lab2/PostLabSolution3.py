b7b3d8f369055484759ae98b282b0675a0027cf4
Peppy <jmarkusp20@gmail.com>
2025-01-03 17:44:10 +0800

    Update PostLabSolution3.py

--- Lab2/PostLabSolution3.py
+++ Lab2/PostLabSolution3.py
@@ -1,7 +1,7 @@
 import random
 
-NounFile = open("/workspaces/GithubLabExerRepo/Lab2/nouns.txt")
-Nouns = NounFile.read().splitlines()
+NounFile = open("/workspaces/GithubLabExerRepo/Lab2/nouns.txt") #Copied the path of the txt file and pasted.
+Nouns = NounFile.read().splitlines() #Reads the file and returns a LIST for every "Enter key" entered in the txt file.
 VerbFile = open("/workspaces/GithubLabExerRepo/Lab2/verbs.txt")
 Verbs = VerbFile.read().splitlines()
 ArticleFile = open("/workspaces/GithubLabExerRepo/Lab2/articles.txt")
@@ -9,14 +9,19 @@ Articles = ArticleFile.read().splitlines()
 PrepositionFile = open("/workspaces/GithubLabExerRepo/Lab2/preposition.txt")
 Prepositions = PrepositionFile.read().splitlines()
 
+Nouns_T = tuple(Nouns) #From list it will be converted into a tuple.
+Verbs_T = tuple(Verbs)
+Articles_T = tuple(Articles)
+Prepositions_T = tuple(Prepositions)
+
 def make_sentence():
     return noun_sentence() + " " + verb_sentence()
 def noun_sentence():
-    return random.choice(Articles) + " " + random.choice(Nouns)
+    return random.choice(Articles_T) + " " + random.choice(Nouns_T)
 def verb_sentence():
-    return random.choice(Verbs) + " " + noun_sentence() + " " + preposition()
+    return random.choice(Verbs_T) + " " + noun_sentence() + " " + preposition()
 def preposition():
-    return random.choice(Prepositions) + " " + noun_sentence()
+    return random.choice(Prepositions_T) + " " + noun_sentence()
   
 answer = int(input("How many sentences? "))
 for i in range(answer):
